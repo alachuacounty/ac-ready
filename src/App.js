48c785889180c1079ca2cb37547eb6ec983ce777
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2021-10-28 15:19:26 -0400

    Set up files for each component in ac-ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navigation from './Components/Navigation';
+import Hero from './Components/Hero';
+import PreparedSection from './Components/PreparedSection';
+import NotifiedSection from './Components/NotifiedSection';
+import StickerSection from './Components/StickerSection';
+import Footer from './Components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return <div className='grid-container'></div>;
+  }
 }
 
 export default App;
---
+++ src/Components/NotifiedSection/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NotifiedSection() {
+  return <div></div>;
+}
+
+export default NotifiedSection;
