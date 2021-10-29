cc90d11f8c9db9a1d06620df62ac6526b766777f
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2021-10-29 14:18:12 -0400

    working on prepare image grid section

--- src/Components/PreparedSection/prepared.jsx
+++ src/Components/PreparedSection/prepared.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './prepared.module.css';
+import imageleft from '../../images/Group19.png';
+import imageright from '../../images/Group20.png';
 function PreparedSection() {
   return (
     <div>
       <section className={styles.prepared}>
-        <p>the prepared is going fourth</p>
+        <h1 className={styles.prepared_header}>Prepare</h1>
+        <div className={styles.image_grid}></div>
       </section>
     </div>
   );
