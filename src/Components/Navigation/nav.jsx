ab8aa6489507d13bd6a28575f71201f07354e273
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2021-10-29 10:14:27 -0400

    added content for navigation, moving to hero

--- src/Components/Navigation/nav.jsx
+++ src/Components/Navigation/nav.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import styles from './nav.module.css';
+import acr_logo from '../../images/ACR_logo.png';
 function Navigation(props) {
   return (
     <div>
       <section className={styles.navigation}>
-        <p>the navigation is going first</p>
+        <img className={styles.acr_logo} src={acr_logo} />
+
+        <ul className={styles.navlinks}>
+          <li className={styles.links}>
+            <a href=''>get notified</a>
+          </li>
+          <li className={styles.links}>
+            <a href=''>prepare</a>
+          </li>
+          <li className={styles.links}>
+            <a href=''>acr stickers</a>
+          </li>
+        </ul>
       </section>
     </div>
   );
