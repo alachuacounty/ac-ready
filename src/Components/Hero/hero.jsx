e612504b9c803bbfd55eff08e2c45238dc381c2a
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2021-11-01 13:38:49 -0400

    replaced background image in css with img src in code

--- src/Components/Hero/hero.jsx
+++ src/Components/Hero/hero.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './hero.module.css';
-import hero_img from '../../images/Hero.png';
+import hero from '../../images/Hero.png';
 function Hero() {
   return (
     <div>
       <section className={styles.hero}>
-        <img className={styles.hero_img} />
+        <img className={styles.hero_img} src={hero} />
       </section>
     </div>
   );
