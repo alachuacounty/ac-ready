6dbeb38f508c31ba6ab6119a93a3fa5a1a7e7b69
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2021-11-03 09:31:54 -0400

    added hero image, working on text logic

--- src/Components/Hero/hero.jsx
+++ src/Components/Hero/hero.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from './hero.module.css';
-import hero from '../../images/Hero.png';
+import hero from '../../images/Image_13.png';
 function Hero() {
   return (
     <div>
       <section className={styles.hero}>
-        {/* <img className={styles.hero_img} src={hero} /> */}
+        <img className={styles.hero_img} src={hero} />
+
+        <div className={styles.hero_text}>
+          <h3>hero text going in here </h3>
+          <br />
+        </div>
       </section>
     </div>
   );
