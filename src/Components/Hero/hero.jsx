cad29f51ac3666403e9749d3344a256b0df38a13
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2021-11-16 11:31:54 -0500

    working on images

--- src/Components/Hero/hero.jsx
+++ src/Components/Hero/hero.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './hero.module.css';
 import hero from '../../images/family_hero.png';
+import hero2 from '../../images/family_hero_dark.png';
 function Hero() {
   return (
     <div>
       <section className={styles.hero}>
         <img
           className={styles.hero_img}
-          src={hero}
+          src={hero2}
           alt='Hero Image of a family of four'
         />
 
