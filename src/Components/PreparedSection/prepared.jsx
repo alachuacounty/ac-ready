3adff243e99d72aa15baf9be13d2bda8915155d8
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2021-11-17 10:02:40 -0500

    pushing image

--- src/Components/PreparedSection/prepared.jsx
+++ src/Components/PreparedSection/prepared.jsx
@@ -32,7 +32,7 @@ function PreparedSection() {
 
         <div className={styles.fema_section}>
           <div className={styles.fema_wrapper}>
-            <img className={styles.fema_image} src={imageleftblack} alt='' />
+            <img className={styles.fema_image} src={imageright} alt='' />
           </div>
           <div className={styles.fema_text}>
             <h2 className={styles.fema_header}>FEMA Disaster Supply Kit</h2>
