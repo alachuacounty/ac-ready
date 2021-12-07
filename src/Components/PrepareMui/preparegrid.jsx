0bbdbecd7b37f2b05eb466d18fcf784310187478
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2021-12-07 15:41:15 -0500

    inserted content for prepare section

--- src/Components/PrepareMui/preparegrid.jsx
+++ src/Components/PrepareMui/preparegrid.jsx
@@ -6,30 +6,13 @@ import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles({
   wrapper: {
-    background: 'magenta',
+
     height: 'auto',
   },
-  left: {
-    background: 'red',
-  },
-  middle: {
-    background: 'pink',
-  },
-  right: {
-    background: 'blue',
-  },
-  leftinner: {
-    background: 'green',
-  },
-  rightinner: {
-    background: 'purple',
-  },
-  imgleft: {
-    background: 'orange',
-  },
-  textright: {
-    height: '260px',
-    background: 'pink',
+ 
+  img:{
+    maxWidth:'350px',
+    width: '100%',
   },
 });
 
@@ -38,6 +21,8 @@ export default function PreparedGrid() {
   return (
     <div>
       <Grid container className={classes.wrapper}>
+        <Grid item container p={2}justifyContent='center'>
+           <Typography variant='h4'>PREPARE</Typography></Grid>
         <Grid item xs={0} lg={1} className={classes.left}></Grid>
         <Grid item container lg={10} className={classes.middle}>
           <Grid item lg={12} className={classes.leftinner} container>
@@ -48,7 +33,7 @@ export default function PreparedGrid() {
               className={classes.imgleft}
               justifyContent='center'
             >
-              <img justify='center' src={imageleftblack} />
+              <img className={classes.img} src={imageleftblack} />
             </Grid>
             <Grid item className={classes.textright} p={2} xs={12} lg={6}>
               <Typography variant='h5'>
@@ -61,11 +46,20 @@ export default function PreparedGrid() {
               <Link> VISIT ALACHUA COUNTY EMERGENCY MANAGEMENT WEBSITE</Link>
             </Grid>
           </Grid>
-          <Grid item lg={12} className={classes.leftinner} container>
-            <Grid item className={classes.textright} xs={12} lg={6}></Grid>
+          <Grid item lg={12} className={classes.leftinner} justifyContent='center' container>
+          <Grid item className={classes.textright} p={2} xs={12} lg={6}>
+              <Typography variant='h5'>
+                Alachua County Emergency Management
+              </Typography>
+              <Typography variant='p'>
+                Visit the Emergency Management Website for more preparedness
+                information and other resources
+              </Typography>
+              <Link> VISIT ALACHUA COUNTY EMERGENCY MANAGEMENT WEBSITE</Link>
+            </Grid>
             <Grid item className={classes.imgleft} xs={12} lg={6}>
               {' '}
-              <img textAlign='center' src={imageleftblack} />
+              <img className={classes.img} src={imageleftblack} />
             </Grid>
           </Grid>
         </Grid>
