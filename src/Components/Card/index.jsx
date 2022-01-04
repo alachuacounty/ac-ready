c24d1ef28ae119eec5795018596a350dbaeca5b2
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2022-01-04 10:27:44 -0500

    Updated content for card component

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -17,7 +17,7 @@ export default function LocationCard() {
   const classes = useStyles();
   return (
     <div>
-      <Grid item justify='center' width='100%' lg={4}>
+      <Grid item justify='center' width='100%' lg={12}>
         <Card>
           <CardHeader className={classes.headerColor} title='Micanopy' />
           <CardContent>
@@ -25,21 +25,63 @@ export default function LocationCard() {
           </CardContent>
         </Card>
         <Card>
-          <CardHeader className={classes.headerColor} title='Micanopy' />
+          <CardHeader className={classes.headerColor} title='Hawthorne' />
           <CardContent>
-            <Typography>- Micanopy Town Hall</Typography>
+            <Typography>- Hawthorne Town Hall</Typography>
+            <Typography>- MMPS Walk-in Clinic</Typography>
           </CardContent>
         </Card>
         <Card>
-          <CardHeader className={classes.headerColor} title='Micanopy' />
+          <CardHeader className={classes.headerColor} title='Gainesville' />
           <CardContent>
-            <Typography>- Micanopy Town Hall</Typography>
+            <Typography>- Alachua County Emergency Management</Typography>
+            <Typography>- Alachua County Library Headquarters</Typography>
+            <Typography>- Public & General</Typography>
+            <Typography>- Satchel’s</Typography>
+            <Typography>- SubLIME Tacos</Typography>
+            <Typography>- Curia on the Drag</Typography>
+            <Typography>- Satch Squared</Typography>
+            <Typography>- Swamp Head</Typography>
           </CardContent>
         </Card>
         <Card>
-          <CardHeader className={classes.headerColor} title='Micanopy' />
+          <CardHeader className={classes.headerColor} title='Archer' />
           <CardContent>
-            <Typography>- Micanopy Town Hall</Typography>
+            <Typography>- Archer City Hall</Typography>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className={classes.headerColor} title='Waldo' />
+          <CardContent>
+            <Typography>- Waldo City Hall</Typography>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className={classes.headerColor} title='Newberry' />
+          <CardContent>
+            <Typography>- Easton Newberry Archery Center</Typography>
+            <Typography>- Newberry City Hall</Typography>
+            <Typography>- Villaggio’s Pizzeria</Typography>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className={classes.headerColor} title='La Crosse' />
+          <CardContent>
+            <Typography>- La Crosse Town Hall</Typography>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className={classes.headerColor} title='Alachua' />
+          <CardContent>
+            <Typography>- Alachua City Hall</Typography>
+            <Typography>- Alachua County Library – Alachua Branch</Typography>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className={classes.headerColor} title='High Springs' />
+          <CardContent>
+            <Typography>- High Springs City Hall</Typography>
+            <Typography>- High Springs Brewing Company</Typography>
           </CardContent>
         </Card>
       </Grid>
