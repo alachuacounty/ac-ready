1eb77e671adbc7403fe5f36b469841896a6a8352
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2022-01-20 10:21:03 -0500

    deployment before tweaking prepared

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends React.Component {
           <NavBarGrid />
           <HeroGrid />
           <NotifyGrid />
-          {/* <PreparedGrid /> */}
+          <PreparedGrid />
           <StickerGrid />
           <FooterGrid />
         </div>
