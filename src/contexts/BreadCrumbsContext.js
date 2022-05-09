2b641830f7d8889616441200b58cdf8bcd1cfe52
Nishant Maurya <nish2493@rocketmail.com>
2022-05-09 13:14:22 -0400

    Breadcrumbs error fix

--- src/contexts/BreadCrumbsContext.js
+++ src/contexts/BreadCrumbsContext.js
@@ -1,28 +1,27 @@
 import React, { createContext, useEffect, useState } from 'react';
 import BreadCrumbs from '../components/Branding/BreadCrumbs';
 
-export const breadCrumbsContext = createContext({});
+export const breadCrumbsContext = createContext([]);
+
+const breadCrumbsDef = [
+  { crumb: 'ALACHUA COUNTY READY HOME', link: '/' },
+  { crumb: 'INCIDENTS', link: '/incidents' },
+];
 
 export default function BreadCrumbsContext({ children }) {
-    const breadCrumbsDef = [{ crumb: 'ALACHUA COUNTY READY HOME', link: '/' }, { crumb: 'INCIDENTS', link: '/incidents' }];
-    const [breadCrumbs, setBreadCrumbs] = useState([]);
+  const [breadCrumbs, setBreadCrumbs] = useState(breadCrumbsDef);
 
-    const pushBreadCrumbs = (nextCrumb) => {
-        if (nextCrumb.crumb && nextCrumb.link) setBreadCrumbs(breadCrumbs.push({ crumb: nextCrumb.crumb, link: nextCrumb.link }));
-        else setBreadCrumbs(breadCrumbsDef);
-    };
+  const pushBreadCrumbs = (nextCrumb) => {
+    if (nextCrumb.crumb && nextCrumb.link)
+      setBreadCrumbs([...breadCrumbs, nextCrumb]);
+    else setBreadCrumbs(breadCrumbsDef);
+  };
 
-    useEffect(() => {
-        //setBreadCrumbs(breadCrumbsDef);
-        //re-render when breadCrumbs change or pushBreadCrumbs is invoked
-    }, [breadCrumbs]);
-
-    return (
-        <breadCrumbsContext.Provider
-            value={{ breadCrumbs: breadCrumbs, pushBreadCrumbs: pushBreadCrumbs }}
-        >
-            {children}
-        </breadCrumbsContext.Provider>
-    );
+  return (
+    <breadCrumbsContext.Provider
+      value={{ breadCrumbs: breadCrumbs, pushBreadCrumbs: pushBreadCrumbs }}
+    >
+      {children}
+    </breadCrumbsContext.Provider>
+  );
 }
-
--- src/pages/Prepare.js
+++ src/pages/Prepare.js
@@ -3,20 +3,19 @@ import React, { useContext, useEffect } from 'react';
 import { titleContext } from '../contexts/TitleContext';
 import { breadCrumbsContext } from '../contexts/BreadCrumbsContext';
 
-
-
 export default function Prepare() {
   const { pageTitle, updatePageTitle } = useContext(titleContext);
   const { breadCrumbs, pushBreadCrumbs } = useContext(breadCrumbsContext);
 
   useEffect(() => {
     updatePageTitle('Prepare');
-    // console.log("crumbs" + breadCrumbs);
-    //pushBreadCrumbs({ crumb: 'Prepare', link: '/prepare' });
+    console.log('crumbs' + breadCrumbs);
+    pushBreadCrumbs({ crumb: 'Prepare', link: '/prepare' });
   }, []);
 
   return (
     <>
-      <div>{pageTitle} {breadCrumbs} </div>
-    </>);
+      <div>{pageTitle}</div>
+    </>
+  );
 }
