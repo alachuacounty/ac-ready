d6f3fb68443a33aba97e71752051ecf41910fc92
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2022-05-11 13:41:01 -0400

    added nhcmaps section, created latest updates section

--- src/pages/IncidentHome.js
+++ src/pages/IncidentHome.js
@@ -2,19 +2,22 @@ import React, { useContext, useEffect } from 'react';
 
 import { titleContext } from '../contexts/TitleContext';
 import { breadCrumbsContext } from '../contexts/BreadCrumbsContext';
+import NHCMaps from '../components/IncidentHome/NHCMaps';
 
 export default function IncidentHome() {
-    const { pageTitle, updatePageTitle } = useContext(titleContext);
-    const { breadCrumbs, pushBreadCrumbs } = useContext(breadCrumbsContext);
+  const { pageTitle, updatePageTitle } = useContext(titleContext);
+  const { breadCrumbs, pushBreadCrumbs } = useContext(breadCrumbsContext);
 
-    useEffect(() => {
-        updatePageTitle('Hurricane Elsa | Home');
-    }, []);
+  useEffect(() => {
+    updatePageTitle('Hurricane Elsa | Home');
+  }, []);
 
-    return (
-        <>
-            <div>{pageTitle}</div>
-
-        </>
-    );
+  return (
+    <>
+      <div>
+        {pageTitle}
+        <NHCMaps />
+      </div>
+    </>
+  );
 }
