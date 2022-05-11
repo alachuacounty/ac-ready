f420e4ef24e3e90a6262078260551f32187a04da
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2022-05-11 16:25:59 -0400

    nhc maps grid

--- src/components/IncidentHome/NHCMaps.js
+++ src/components/IncidentHome/NHCMaps.js
@@ -12,97 +12,100 @@ const useStyles = makeStyles((theme) => ({}));
 export default function NHCMaps() {
   return (
     <div>
-      <Grid container xs={12} spacing={3}>
-        <Grid item textAlign='left' xs={12} p={4}>
+      <Grid container xs={12} spacing={3} justifyContent='center'>
+        <Grid item xs={12} p={4}>
           <Typography variant='h5' textAlign='left'>
             National Hurricane Center Maps
           </Typography>
         </Grid>
-        <Grid
-          item
-          xs={12}
-          sm={12}
-          md={6}
-          lg={6}
-          display='flex'
-          justifyContent='center'
-        >
-          <Card sx={{ maxWidth: 450, height: '100%' }} elevation={5}>
-            <CardActionArea>
-              <CardMedia
-                component='img'
-                height='140'
-                image='/static/images/cards/contemplative-reptile.jpg'
-                alt='green iguana'
-              />
-              <CardContent>
-                <Typography
-                  gutterBottom
-                  variant='h5'
-                  textAlign='left'
-                  component='div'
-                >
-                  Hurricane Force Wind Speed Probabilities
-                </Typography>
-                <Typography
-                  textAlign='left'
-                  variant='body2'
-                  color='text.secondary'
-                >
-                  The probability of sustained surface wind speeds equal to or
-                  exceeding 34kt (39 mph)
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size='small' color='primary'>
-                Learn More
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          sm={12}
-          md={6}
-          lg={6}
-          display='flex'
-          justifyContent='center'
-        >
-          <Card sx={{ maxWidth: 450, height: '100%' }}>
-            <CardActionArea>
-              <CardMedia
-                component='img'
-                height='140'
-                image='/static/images/cards/contemplative-reptile.jpg'
-                alt='green iguana'
-              />
-              <CardContent>
-                <Typography
-                  gutterBottom
-                  variant='h5'
-                  textAlign='left'
-                  component='div'
-                >
-                  Five-Day Storm Forecast Cone
-                </Typography>
-                <Typography
-                  variant='body2'
-                  textAlign='left'
-                  color='text.secondary'
-                >
-                  Approximate Representation of coastal areas under a hurricane
-                  watch, tropical storm warning and tropical storm watch.
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size='small' color='primary'>
-                Learn More
-              </Button>
-            </CardActions>
-          </Card>
+        <Grid container spacing={1} justifyContent='center'>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={6}
+            lg={6}
+            display='flex'
+            justifyContent='center'
+          >
+            <Card sx={{ maxWidth: 450, height: '600' }} elevation={5}>
+              <CardActionArea>
+                <CardMedia
+                  component='img'
+                  height='140'
+                  image='/static/images/cards/contemplative-reptile.jpg'
+                  alt='green iguana'
+                />
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant='h5'
+                    textAlign='left'
+                    component='div'
+                  >
+                    Hurricane Force Wind Speed Probabilities
+                  </Typography>
+                  <Typography
+                    textAlign='left'
+                    variant='body2'
+                    color='text.secondary'
+                  >
+                    The probability of sustained surface wind speeds equal to or
+                    exceeding 34kt (39 mph)
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size='small' color='primary'>
+                  Learn More
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={6}
+            lg={6}
+            display='flex'
+            justifyContent='center'
+          >
+            <Card sx={{ maxWidth: 450, height: '100%' }}>
+              <CardActionArea>
+                <CardMedia
+                  component='img'
+                  height='140'
+                  image='/static/images/cards/contemplative-reptile.jpg'
+                  alt='green iguana'
+                />
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant='h5'
+                    textAlign='left'
+                    component='div'
+                  >
+                    Five-Day Storm Forecast Cone
+                  </Typography>
+                  <Typography
+                    variant='body2'
+                    textAlign='left'
+                    color='text.secondary'
+                  >
+                    Approximate Representation of coastal areas under a
+                    hurricane watch, tropical storm warning and tropical storm
+                    watch.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size='small' color='primary'>
+                  Learn More
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
         </Grid>
       </Grid>
     </div>
