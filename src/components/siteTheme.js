dfd725ca0580273aa4beaedbb4b1f70d6eb0f9d4
ashvini-patel <>
2022-05-12 13:16:07 -0400

    clean up sitetheme

--- src/components/siteTheme.js
+++ src/components/siteTheme.js
@@ -3,20 +3,33 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     darkblue: {
-      main: '#12264c',
-      light: '#CFE3E3',
+      main: '#12274B',
+      light: '#1B5B98',
+      lightest: '#1968b1',
     },
     middleblue: {
       main: '#1968b2',
+      light: '#4A97D2'
     },
     lightblue: {
-      main: '#1976d2',
+      main: '#BFD8F2',
+      light: '#D0EDFC',
     },
     white: {
-      main: '#FFFFFF!important',
+      main: '#FFFFFF',
+    },
+    black: {
+      main: '#000000',
     },
     grey: {
       main: '#707070',
+      light: '#DBDBDB',
+      lightest: '#E8E8E8',
+      dark: '#222222',
+    },
+    yellow: {
+      main: '#EFD06A',
+      dark: '#D3933D',
     }
   },
   sectionpadding: {
