02c751fad2fddce398d45a9899d7ebea8a7c4317
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2022-05-13 16:12:03 -0400

    added info icon and created modal file

--- src/components/EmergencyBanner.js
+++ src/components/EmergencyBanner.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Button, Link, Typography, Grid } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import theme from './siteTheme';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 const useStyles = makeStyles((theme) => ({
   title: {
     textTransform: 'uppercase',
@@ -21,7 +22,8 @@ export default function EmergencyBanner() {
         backgroundColor={theme.palette.yellow.main}
         height='70px'
       >
-        <Typography variant='h5' textAlign='left' className={classes.title}>
+        <InfoOutlinedIcon textAlign='center' />
+        <Typography variant='h6' textAlign='left' className={classes.title}>
           Currently Monitoring Hurricane Elsa - View Important information about
           this storm
         </Typography>
