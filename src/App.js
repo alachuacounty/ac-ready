9da278894a464f0faa4ad03bedd21bfdce73711d
ashvini-patel <>
2022-05-25 15:44:44 -0400

    Incidents context
    context from monday, dynamic routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { Route, Routes } from 'react-router-dom';
 
 import theme from './components/siteTheme';
-import { incidentsContext } from './contexts/IncidentsContext';
+import { incidentsContext } from './contexts/IncidentsContext'
 
 import Appbar from './components/Branding/Appbar';
 import Home from './pages/Home';
@@ -22,48 +22,89 @@ import Advisory from './pages/Advisory';
 import ReportDamage from './pages/ReportDamage';
 
 export default function App() {
+
   const incidents = useContext(incidentsContext);
+  //const [IncidentsFetched, setIncidentsFecthed] = useState(false);
+  //const incidents = incidentsContext();
+
+  console.log("check context");
+  console.log(incidents);
+
+  /*useEffect(() => {
+    console.log("ran useffect on app");
+    console.log(incidents.length)
+    if (incidents.length > 0) {
+      setIncidentsFecthed(true);
+      console.log("fetched");
+    }
+  }, [incidents]);*/
 
   return (
     <ThemeProvider theme={theme}>
       <Appbar>
+
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route
-            path='/incidents/elsa/emergencyorders'
-            element={<EmergencyOrder />}
-          />
-          <Route path='/incidents/elsa/faqs' element={<FAQ />} />
-          <Route
-            path='/incidents/elsa/importantlinks'
-            element={<ImportantLinksPage />}
-          />
-          <Route path='/incidents/elsa/sandbags' element={<SandbagPage />} />
-          <Route path='/incidents/elsa/shelters' element={<Shelter />} />
-          <Route
-            path='/incidents/elsa/roadclosures'
-            element={<RoadClosures />}
-          />
-          <Route
-            path='/incidents/elsa/reportdamages'
-            element={<ReportDamage />}
-          />
-          <Route path='/incidents/elsa' element={<IncidentHome />} />
-          <Route path='/incidents/elsa/advisories' element={<Advisories />} />
-          <Route
-            path='/incidents/elsa/advisories/:UpdateID'
-            element={<Advisory />}
-          />
+
+
           {incidents.length > 0 && (
             <>
               <Route path='incidents' element={<Incidents />} />
               {incidents.map((incident) => (
-                <Route
-                  path={`incidents/${incident.urlName}`}
-                  element={
-                    <Incident name={incident.name} pages={incident.pages} />
-                  }
-                />
+                <>
+                  {/* 
+                    <Route
+                    path={`incidents/${incident.urlName}`}
+                    element={
+                      <Incident name={incident.name} pages={incident.pages} />
+                    }
+                  />
+                  */}
+
+                  <Route
+                    path={'/incidents/' + incident.urlName + '/emergencyorders'}
+                    element={<EmergencyOrder />}
+                  />
+
+                  <Route
+                    path={'/incidents/' + incident.urlName + '/faqs'}
+                    element={<FAQ />}
+                  />
+
+                  <Route
+                    path={'/incidents/' + incident.urlName + '/importantlinks'}
+                    element={<ImportantLinksPage />}
+                  />
+
+                  <Route
+                    path={'/incidents/' + incident.urlName + '/sandbags'}
+                    element={<SandbagPage />}
+                  />
+                  <Route
+                    path={'/incidents/' + incident.urlName + '/shelters'}
+                    element={<Shelter />}
+                  />
+                  <Route
+                    path={'/incidents/' + incident.urlName + '/roadclosures'}
+                    element={<RoadClosures />}
+                  />
+                  <Route
+                    path={'/incidents/' + incident.urlName + '/reportdamages'}
+                    element={<ReportDamage />}
+                  />
+                  <Route
+                    path={'/incidents/' + incident.urlName}
+                    element={<IncidentHome />}
+                  />
+                  <Route
+                    path={'/incidents/' + incident.urlName + '/advisories'}
+                    element={<Advisories />}
+                  />
+                  <Route
+                    path={'/incidents/' + incident.urlName + '/advisories/:UpdateID'}
+                    element={<Advisory />}
+                  />
+                </>
               ))}
             </>
           )}
--- src/contexts/IncidentsContext.js
+++ src/contexts/IncidentsContext.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import axios from 'axios';
 import moment from 'moment';
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState, useLayoutEffect } from 'react';
 
 export const incidentsContext = createContext([]);
 
@@ -12,10 +12,10 @@ export default function IncidentsContext({ children }) {
     setIncidents(incidentsArray);
   };
 
-  const getAdvisoryData = async () => {
+  const getAdvisoryData = async (incidentID, incidentURL) => {
     try {
       const result = await axios.get(
-        `https://ads86.alachuacounty.us/incidents-api/advisory/active`
+        `https://ads86.alachuacounty.us/incidents-api/advisory/active` + `/` + incidentID
       );
       if (result && result.data && result.data[0].length) {
         const tempAdvisories = [];
@@ -33,7 +33,7 @@ export default function IncidentsContext({ children }) {
           );
           tempAdvisory.title = advisory.AdvisoryName;
           tempAdvisory.desc = advisory.Blurb;
-          tempAdvisory.link = `/incidents/elsa/advisories/${advisory.MondayID}`;
+          tempAdvisory.link = `/incidents/` + incidentURL + `/advisories/${advisory.MondayID}`;
           tempAdvisories.push(tempAdvisory);
         });
         return tempAdvisories;
@@ -43,42 +43,95 @@ export default function IncidentsContext({ children }) {
     }
   };
 
-  const initialLoad = async () => {
-    const advisoryData = await getAdvisoryData();
-    updateIncidents([
-      {
-        name: 'Hurricane Elsa',
-        urlName: 'elsa',
-        eventType: 'Hurricane',
-        advisories: advisoryData,
-        pages: [
-          { id: 680683111, name: 'Frequently Asked Questions', position: 6 },
-          { id: 599761016, name: 'Sandbags Locations', position: 2 },
-          { id: 599761016, name: 'Shelter Locations', position: 1 },
-          { id: 599761016, name: 'Important Links', position: 5 },
-          { id: 599761016, name: 'Recent Advisories', position: 3 },
-          { id: 599761016, name: 'Emergency Orders', position: 4 },
-        ],
-      },
-      {
-        name: 'Hurricane Dorian',
-        urlName: 'dorian',
-        eventType: 'Hurricane',
-        advisories: advisoryData,
-        pages: [
-          { id: 680683111, name: 'Frequently Asked Questions', position: 6 },
-          { id: 599761016, name: 'Sandbags Locations', position: 2 },
-          { id: 599761016, name: 'Shelter Locations', position: 1 },
-          { id: 599761016, name: 'Important Links', position: 5 },
-          { id: 599761016, name: 'Recent Advisories', position: 3 },
-          { id: 599761016, name: 'Emergency Orders', position: 4 },
-        ],
-      },
-    ]);
+
+  const getIncidentPages = async (incidentID, incidentURL) => {
+
+    try {
+      const result = await axios.get(
+        `https://ads86.alachuacounty.us/incidents-api/incidents/activepages/` + incidentID
+      );
+      if (result && result.data && result.data[0].length) {
+        const IncidentPages = [];
+        IncidentPages.push({ title: "Home", link: "/incident/" + incidentURL });
+        const prepareSubmenu = [];
+        const updatesSubmenu = [];
+
+        result.data[0].forEach((page) => {
+
+          const incidentPage = {};
+          incidentPage.boardID = page.BoardID;
+          incidentPage.name = page.PageName;
+          incidentPage.title = page.PageName;
+          incidentPage.link = "/incidents/" + incidentURL + "/" + page.PageName;
+
+          if (page.Category.toLowerCase() === "do not group") {
+            IncidentPages.push(incidentPage);
+          } else if (page.Category.toLowerCase() === "prepare") {
+            prepareSubmenu.push(incidentPage);
+          }
+          else if (page.Category.toLowerCase() === "updates") {
+            updatesSubmenu.push(incidentPage);
+          }
+
+        });
+
+        if (prepareSubmenu.length !== 0) {
+          IncidentPages.push({ title: "Prepare", submenu: prepareSubmenu });
+        }
+        if (updatesSubmenu.length !== 0) {
+          IncidentPages.push({ title: "Updates", submenu: updatesSubmenu });
+        }
+
+        return IncidentPages;
+      }
+      else {
+        return [];
+      }
+    } catch (error) {
+      console.log({ error });
+    }
   };
 
-  // Yet to Implement fetch incidents from monday board
-  useEffect(() => {
+
+  const getActiveIncidents = async () => {
+    try {
+      const result = await axios.get(
+        `https://ads86.alachuacounty.us/incidents-api/incidents/active`
+      );
+      if (result && result.data && result.data[0].length) {
+
+        const activeIncidents = [];
+
+        for (const incident of result.data[0]) {
+          const incidentData = {};
+          incidentData.incidentID = incident.MondayID;
+          incidentData.name = incident.IncidentName;
+          incidentData.urlName = incident.IncidentName.replace(" ", '').toLowerCase();
+          incidentData.damageReportURL = incident.DamageReportURL;
+          incidentData.imageLink1 = incident.Link1;
+          incidentData.imageLink2 = incident.Link2;
+          incidentData.pages = await getIncidentPages(incidentData.incidentID, incidentData.urlName);
+          incidentData.advisories = await getAdvisoryData(incidentData.incidentID, incidentData.urlName);
+          activeIncidents.push(incidentData);
+
+        }
+        return activeIncidents;
+      } else {
+        return [];
+      }
+    } catch (error) {
+      console.log({ error });
+    }
+  }
+
+  const initialLoad = async () => {
+
+    const activeIncidents = await getActiveIncidents();
+    //console.log(activeIncidents);
+    updateIncidents(activeIncidents);
+  };
+
+  useLayoutEffect(() => {
     initialLoad();
   }, []);
 
