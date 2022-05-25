4b99e4c89b762c84856220a09767f45a284c7efd
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2022-05-25 14:30:05 -0400

    updated grid for current incidents page

--- src/pages/Incidents.js
+++ src/pages/Incidents.js
@@ -10,6 +10,7 @@ import hurricaneImage from '../images/hurricane.svg';
 const cardStyles = {
   background: (theme) => theme.palette.lightblue.light,
   mx: { xs: '2%', sm: '8% !important' },
+
   mb: '2% !important',
   display: 'flex',
   alignItems: 'center',
@@ -52,7 +53,10 @@ export default function Incidents() {
         incidents.map((incident, index) => (
           <Grid
             item
-            xs={3}
+            xs={12}
+            sm={4}
+            md={6}
+            lg={3}
             sx={cardStyles}
             key={index}
             onClick={() => navigateToPage(incident.urlName)}
@@ -64,7 +68,10 @@ export default function Incidents() {
         ))}
       <Grid
         item
-        xs={3}
+        xs={12}
+        sm={4}
+        md={6}
+        lg={3}
         sx={cardStyles}
         key={99}
         onClick={() =>
