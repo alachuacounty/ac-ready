80ce71b43dd5c88a2f7f52aca8ce14ea95752949
Nishant Maurya <nish2493@rocketmail.com>
2022-05-27 12:07:39 -0400

    We are being Smart

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -11,24 +11,23 @@ export default function NotFound() {
     updatePageTitle('Page Not Found');
   }, []);
 
-  return (
-    <Grid
-      container
-      sx={{ p: 2, height: { xs: '84vh', md: '90vh', lg: '88vh' } }}
-    >
-      <Grid item xs={12} sx={{ pt: 3 }}>
-        <Typography variant='h3' sx={{ pb: 3 }}>
-          Page Not Found
-        </Typography>
-        <Typography variant='h5' sx={{ pb: 1 }}>
-          We apologize, but the page or resource you requested could not be
-          found.
-        </Typography>
-        <Typography variant='h6'>
-          The page or resource you requested may not exist, may have been moved
-          or the address was entered incorrectly.
-        </Typography>
-      </Grid>
-    </Grid>
-  );
+  return null;
+  // <Grid
+  //   container
+  //   sx={{ p: 2, height: { xs: '84vh', md: '90vh', lg: '88vh' } }}
+  // >
+  //   <Grid item xs={12} sx={{ pt: 3 }}>
+  //     <Typography variant='h3' sx={{ pb: 3 }}>
+  //       Page Not Found
+  //     </Typography>
+  //     <Typography variant='h5' sx={{ pb: 1 }}>
+  //       We apologize, but the page or resource you requested could not be
+  //       found.
+  //     </Typography>
+  //     <Typography variant='h6'>
+  //       The page or resource you requested may not exist, may have been moved
+  //       or the address was entered incorrectly.
+  //     </Typography>
+  //   </Grid>
+  // </Grid>
 }
